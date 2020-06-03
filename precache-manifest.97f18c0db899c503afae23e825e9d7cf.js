c008d1ee4d79430fb1a5693ae64b2ad92a43d436
Utkarsh Mehta <utkarsh@tarkalabs.com>
2020-06-03 16:05:12 +0530

    Updates

---
+++ precache-manifest.97f18c0db899c503afae23e825e9d7cf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1341e5b6ca7632a7731ea5470768b45b",
+    "url": "/webrtc-react/index.html"
+  },
+  {
+    "revision": "8ee3d0fa0824ad71963f",
+    "url": "/webrtc-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9b0b790726f1e243a6f5",
+    "url": "/webrtc-react/static/js/2.d563e810.chunk.js"
+  },
+  {
+    "revision": "e6191615acbaa88870aef4cd0368e4ec",
+    "url": "/webrtc-react/static/js/2.d563e810.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ee3d0fa0824ad71963f",
+    "url": "/webrtc-react/static/js/main.50d3b8d6.chunk.js"
+  },
+  {
+    "revision": "981b0198e57262cd8969",
+    "url": "/webrtc-react/static/js/runtime-main.2b631a07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/webrtc-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
