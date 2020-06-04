6dd04ace4acd0e30d57bb5cca480255da193619d
Utkarsh Mehta <utkarsh@tarkalabs.com>
2020-06-04 15:49:28 +0530

    fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var PeerServer = require('peer').PeerServer;
+// var PeerServer = require('peer').PeerServer;
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3001;
@@ -8,18 +8,18 @@ var port = process.env.PORT || 3001;
 app.use(express.static(__dirname + '/simple-peer'));
 
 var expressServer = app.listen(port);
-var io = require('socket.io').listen(expressServer);
+// var io = require('socket.io').listen(expressServer);
 
 console.log('Listening on port', port);
 
-var peerServer = new PeerServer({ port: 9000, path: '/' });
+// var peerServer = new PeerServer({ port: 9000, path: '/' });
 
-peerServer.on('connection', function (id) {
-    //io.emit(Topics.USER_CONNECTED, id);
-    console.log('User connected.');
-});
+// peerServer.on('connection', function (id) {
+//     //io.emit(Topics.USER_CONNECTED, id);
+//     console.log('User connected.');
+// });
 
-peerServer.on('disconnect', function (id) {
-    //io.emit(Topics.USER_DISCONNECTED, id);
-    console.log('User disconnected.');
-});
+// peerServer.on('disconnect', function (id) {
+//     //io.emit(Topics.USER_DISCONNECTED, id);
+//     console.log('User disconnected.');
+// });
