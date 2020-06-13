c80b33a06b006515b6060565f14b44034d86bfb0
Utkarsh Mehta <utkarsh@tarkalabs.com>
2020-06-13 14:00:17 +0530

    revert

--- index.js
+++ index.js
@@ -20,41 +20,6 @@ var srv = app.listen(port, function () {
     console.log('Listening on ' + port)
 })
 
-var peerJSServer = ExpressPeerServer(srv, {
+app.use('/chat', require('peer').ExpressPeerServer(srv, {
     debug: true
-});
-
-app.get("/api/admin/onlineUsers", (req, res) => {
-    try {
-        res.status(200).json({ users: stats.getLiveUsers() });
-    } catch (ex) {
-        res.status(422).json(ex);
-    }
-});
-
-app.use('/chat', peerJSServer);
-
-peerJSServer.on('connection', function (peer) {
-    stats.connected(peer.id);
-    console.log(logInYellow, 'user with ' + peer.id + ' connected');
-});
-
-peerJSServer.on('disconnect', function (peer) {
-    stats.disconnected(peer.id);
-    console.log(logInYellow, 'user with ' + peer.id + ' disconnected');
-});
-
-peerJSServer.on('message', function (peer, message) {
-    if (message) {
-        if (message.type == 'HEARTBEAT') {
-            stats.heartbeat(peer.id);
-        }
-        else {
-            console.log(logInYellow, 'user with ' + peer.id + ' streaming data ' + JSON.stringify(message));
-        }
-    }
-});
-
-peerJSServer.on('error', function (err) {
-    console.log(err);
-});
+}))
