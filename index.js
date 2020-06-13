a66895ad5d8dd08e0222d34feb6e4090eb79b62e
Utkarsh Mehta <utkarsh@tarkalabs.com>
2020-06-13 14:03:18 +0530

    fix

--- index.js
+++ index.js
@@ -13,13 +13,41 @@ app.use(morgan('tiny'))
 
 const port = process.env.PORT || 3001;
 
-app.use('/webrtc-react', express.static(path.join(__dirname, 'build')))
-app.use(express.static(path.join(__dirname, 'build')));
-
-var srv = app.listen(port, function () {
-    console.log('Listening on ' + port)
-})
-
-app.use('/chat', require('peer').ExpressPeerServer(srv, {
+var peerJSServer = ExpressPeerServer(srv, {
     debug: true
-}))
+});
+
+app.get("/api/admin/onlineUsers", (req, res) => {
+    try {
+        res.status(200).json({ users: stats.getLiveUsers() });
+    } catch (ex) {
+        res.status(422).json(ex);
+    }
+});
+
+app.use('/chat', peerJSServer);
+
+peerJSServer.on('connection', function (peer) {
+    stats.connected(peer.id);
+    console.log(logInYellow, 'user with ' + peer.id + ' connected');
+});
+
+peerJSServer.on('disconnect', function (peer) {
+    stats.disconnected(peer.id);
+    console.log(logInYellow, 'user with ' + peer.id + ' disconnected');
+});
+
+peerJSServer.on('message', function (peer, message) {
+    if (message) {
+        if (message.type == 'HEARTBEAT') {
+            stats.heartbeat(peer.id);
+        }
+        else {
+            console.log(logInYellow, 'user with ' + peer.id + ' streaming data ' + JSON.stringify(message));
+        }
+    }
+});
+
+peerJSServer.on('error', function (err) {
+    console.log(err);
+});
